18262b15ff576130835bc41d094f55aaf57b1e22
UTurista <vasco@programador.pt>
2024-02-28 15:22:55 +0000

    Removes unused imports

--- PRDownloader.App/Clients/TorrentInformationClient.cs
+++ PRDownloader.App/Clients/TorrentInformationClient.cs
@@ -3,52 +3,44 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using PRDownloader.App.Models;
 using PRDownloader.App.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
-using Windows.Media.Protection.PlayReady;
-using static Microsoft.Maui.Authentication.AppleSignInAuthenticator;
 
-namespace PRDownloader.App.Clients
+namespace PRDownloader.App.Clients;
+
+public sealed class TorrentInformationClient
 {
-    public sealed class TorrentInformationClient
+    private readonly HttpClient _client;
+    private readonly Uri _VersionUri;
+    private readonly ILogger<TorrentInformationClient> _logger;
+
+    public TorrentInformationClient(HttpClient client, IOptions<TorrentOptions> options, ILogger<TorrentInformationClient> logger)
     {
-        private readonly HttpClient _client;
-        private readonly Uri _VersionUri;
-        private readonly ILogger<TorrentInformationClient> _logger;
+        _client = client;
+        _VersionUri = options.Value.VersionUrl;
+        _logger = logger;
+    }
 
-        public TorrentInformationClient(HttpClient client, IOptions<TorrentOptions> options, ILogger<TorrentInformationClient> logger)
+    public async Task<Result<TorrentInformation>> GetInformationAsync(CancellationToken cancellationToken = default)
+    {
+        var response = await _client.GetAsync(_VersionUri, cancellationToken);
+        if (!response.IsSuccessStatusCode)
         {
-            _client = client;
-            _VersionUri = options.Value.VersionUrl;
-            _logger = logger;
+            return Result.Fail("Unable to fetch information");
         }
 
-        public async Task<Result<TorrentInformation>> GetInformationAsync(CancellationToken cancellationToken = default)
+        try
         {
-            var response = await _client.GetAsync(_VersionUri, cancellationToken);
-            if (!response.IsSuccessStatusCode)
+            var content = await response.Content.ReadAsStringAsync(cancellationToken);
+            var information = JsonSerializer.Deserialize<TorrentInformation>(content);
+            if (information is not null)
             {
-                return Result.Fail("Unable to fetch information");
+                return Result.Ok(information);
             }
-
-            try
-            {
-                var content = await response.Content.ReadAsStringAsync(cancellationToken);
-                var information = JsonSerializer.Deserialize<TorrentInformation>(content);
-                if (information is not null)
-                {
-                    return Result.Ok(information);
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Unable to parse response.");
-            }
-            return Result.Fail("Failed to parse response");
         }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unable to parse response.");
+        }
+        return Result.Fail("Failed to parse response");
     }
 }
